fbb7d323c841ec9442f954e4bee8878bde26903f
Teresa Loafman <teresaloafman@CBL-L84128.local>
2024-01-17 15:32:21 -0700

    Added meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -23,11 +23,21 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 };
 
 export default function App() {
+  const ogImg = "https://thegigguider.com/img/OG_landingPage.png";
   return (
     <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta property="og:title" content="The Gig Guider" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://TheGigGuider.com/" />
+        <meta property="og:image" content={`${ogImg}`} />
+        <meta property="og:site_name" content="The Gig Guider" />
+        <meta
+          property="og:description"
+          content="Enter in your gig details and see if it's worth it!"
+        />
         <Meta />
         <Links />
         {/* <!-- Google tag (gtag.js) --> */}
